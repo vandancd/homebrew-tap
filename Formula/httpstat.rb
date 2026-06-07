18884dc7f394bfeb96014e18e6cacfedaabd48f1
Vandan Desai <vandancd@users.noreply.github.com>
2026-06-06 20:25:55 -0400

    chore: update httpstat to v1.1.1

--- Formula/httpstat.rb
+++ Formula/httpstat.rb
@@ -1,9 +1,9 @@
 class Httpstat < Formula
   desc "HTTP performance measurement tool with waterfall output and trace"
   homepage "https://github.com/vandancd/httpstat"
-  url "https://github.com/vandancd/httpstat/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "e5798964639934065f0c207b90145e5b551b59484ee6b9ac8b2c97a2d40559df"
-  version "1.1.0"
+  url "https://github.com/vandancd/httpstat/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "7074e0c7b5de2a7125af45217b08c55e8c302ee8d7559fbf79939a514b36d650"
+  version "1.1.1"
   license "MIT"
 
   depends_on "go" => :build
